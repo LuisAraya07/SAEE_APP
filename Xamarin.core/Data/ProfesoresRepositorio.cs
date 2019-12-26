155159a368d62d273e91f79f2ed85ce169fa8e77
Heiner Salvatierra Ramírez <Heiner007@outlook.es>
2019-12-26 14:16:34 -0600

    Implementación de repositorio y servicio de Profesores

---
+++ Xamarin.core/Data/ProfesoresRepositorio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.core.Models;
+using Xamarin.core.Services;
+
+namespace Xamarin.core.Data
+{
+    public class ProfesoresRepositorio
+    {
+        private readonly WebServices ws;
+        public ProfesoresRepositorio()
+        {
+            ws = new WebServices();
+        }
+
+        public List<Profesores> Get()
+        {
+            var response = ws.Get(ValuesServices.url + "Profesores");
+
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<List<Profesores>>(response.Content);
+        }
+
+        public Profesores GetGrupo(int id)
+        {
+            var response = ws.Get(ValuesServices.url + "Profesores/" + id);
+            if (response.HttpStatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new ApplicationException("Profesor no encontrado");
+            }
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<Profesores>(response.Content);
+        }
+    }
+}
--- Xamarin.core/Services/ProfesoresServices.cs
+++ Xamarin.core/Services/ProfesoresServices.cs
@@ -9,11 +9,27 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Xamarin.core.Data;
+using Xamarin.core.Models;
 
 namespace Xamarin.core.Services
 {
     public class ProfesoresServices
     {
+        private readonly ProfesoresRepositorio _profesoresR;
+        public ProfesoresServices()
+        {
+            _profesoresR = new ProfesoresRepositorio();
+        }
 
+        public List<Profesores> Get()
+        {
+            return _profesoresR.Get();
+        }
+
+        public Profesores GetGrupo(int id)
+        {
+            return _profesoresR.GetGrupo(id);
+        }
     }
 }
