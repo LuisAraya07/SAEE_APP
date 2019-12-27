a285160807513556fad094095eac1422a6d33216
Heiner Salvatierra Ramírez <Heiner007@outlook.es>
2019-12-26 22:05:17 -0600

    Implementación de Actividad Profesores

---
+++ SAEEAPP/Adaptadores/ProfesoresListAdapter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.core.Models;
+
+namespace SAEEAPP.Adaptadores
+{
+    class ProfesoresListAdapter : BaseAdapter<Profesores>
+    {
+        private Activity _context;
+        private List<Profesores> _profesores;
+
+        public ProfesoresListAdapter(Activity context, List<Profesores> profesores)
+        {
+            _context = context;
+            _profesores = profesores;
+        }
+
+        public override Profesores this[int position] => _profesores[position];
+
+        public override int Count => _profesores.Count;
+
+        public override long GetItemId(int position)
+        {
+            return this[position].Id;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            Profesores profesor = this[position];
+
+            if (convertView == null)
+            {
+                convertView = _context.LayoutInflater.Inflate(Resource.Layout.ProfesoresListRow, null);
+            }
+            convertView.
+                FindViewById<TextView>(Resource.Id.tvNombre).
+                Text = $"{profesor.Nombre} {profesor.PrimerApellido} {profesor.SegundoApellido}";
+            convertView.FindViewById<TextView>(Resource.Id.tvCedula).Text = profesor.Cedula;
+            convertView.FindViewById<TextView>(Resource.Id.tvCorreo).Text = profesor.Correo;
+            return convertView;
+        }
+    }
+}
